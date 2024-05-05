2343495752733e5982ec2da7ff2c38df224b6567
dmarie777 <deimarie777@gmail.com>
2024-05-05 12:31:21 -0500

    Created files for the pattern project and added a rectangle in the sketch.js file

---
+++ Pattern/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(window.innerWidth, windowHeight);
+}
+
+function draw() {
+    background(220);
+
+    //Draw a rectangle 
+    noStroke();
+    fill(2, 64, 89);
+    rect(0, 0, width, height);
+
+
+
+}
