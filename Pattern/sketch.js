17454f76ad5a5a91654efeaebff18e892331536c
dmarie777 <deimarie777@gmail.com>
2024-05-05 13:15:57 -0500

    Drawing lines

--- Pattern/sketch.js
+++ Pattern/sketch.js
@@ -10,6 +10,17 @@ function draw() {
     fill(2, 64, 89);
     rect(0, 0, width, height);
 
+    //Draw a grid diagonal lines
+    for(let j=0; j<height; j+=50) {
+        for(let i=0; i<width; i+=40) {
+            stroke(0, i, j+50, 200);
+            strokeWeight(4);
+
+            stroke(i, i+200, j+50, 200);
+            line(j, i, i, j);
+
+        }
+    }
 
 
 }
