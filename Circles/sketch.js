9e5896557202be356cd128969e240d3b1f1d7e68
dmarie777 <deimarie777@gmail.com>
2024-05-30 23:55:36 -0500

    Added functionality to erase the canvas while moving the mouse

--- Circles/sketch.js
+++ Circles/sketch.js
@@ -3,24 +3,36 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    let cnv = createCanvas(windowWidth, windowHeight);
     background(150);
     colorMode(HSB, 360, 100, 100);
     palette = floor(random(5));
     getColor(floor(random(5)));
-    fill(h, s, b);
     stroke(h, s, b)
-    rad = 5;
+    rad = 40;
+    blendMode(REMOVE);
+    fill(h, s, b);
 }
 
 
 function draw() {
-    circle(mouseX, mouseY, rad);
+    const lerpFunc = (x, y, a) => x * (1 - a) + y * a;
+    const prevX = pmouseX;
+    const prevY = pmouseY;
+
+    const numbCircles = max(abs(mouseX - prevX), abs(mouseY - prevY));
+
+    for (let i = 0; i<numbCircles; i++) {
+        const targetX = lerpFunc(mouseX, prevX, (1 / numbCircles) * i);
+        const targetY = lerpFunc(mouseY, prevY, (1 / numbCircles) * i);
+        print((1 / numbCircles) * i);
+        circle(targetX, targetY, rad);
+    }
+
 }
 
-
 function getColor(col1) {
-
+    print('getcolor')
     h = int(table.get(palette, col1 * 3));
     s = int(table.get(palette, col1 * 3 + 1));
     b = int(table.get(palette, col1 * 3 +2));
