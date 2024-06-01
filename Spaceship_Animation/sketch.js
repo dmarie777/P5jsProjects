17e3d838286e78b15ef9e0a14caa2d4f671e875d
dmarie777 <deimarie777@gmail.com>
2024-05-31 20:34:43 -0500

    Modified the funtionality to draw random stars

--- Spaceship_Animation/sketch.js
+++ Spaceship_Animation/sketch.js
@@ -5,33 +5,15 @@ let speed = 5;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(10);
+
+    for (let i =50; i>1; i--) {
+        ellipse(random(width), random(height), random(3,5));
+    }
 }
 
 function draw(){
-    background(10);
     stroke("none");
 
-    drawSmallStars();
-    drawBigStars();
 }
 
-function drawSmallStars() {
-    stroke('white');
-    ellipse(random(width), random(height),3)
- }
-
-function drawBigStars() { 
-    let gap = 20;
-    let xDim = 2;
-    let yDim = 3;
-    fill(200);
-    ellipse(width *2 / 7, height / 4, xDim, yDim);
-    ellipse(width *2 / 7 + gap, height / 4 + 0.5*height / 4, xDim, yDim);
-
-    ellipse(width *3 / 7, height *2 / 4, xDim, yDim);
-
-    ellipse(width *4 / 7, height / 4, xDim, yDim);
-
-    ellipse(width *6 / 7 -gap, height / 4 -gap, xDim, yDim);
-    ellipse(width *5 / 7 -gap, height / 4 + gap, xDim, yDim);
-}
