bb2782dd2f2ca5a2dd00f9907a4bdc82debd7314
dmarie777 <deimarie777@gmail.com>
2024-06-21 19:32:20 -0500

    Added random rectangles

--- Loops-rect/sketch.js
+++ Loops-rect/sketch.js
@@ -1,4 +1,4 @@
-let h,s,l;
+let h,s,l, palette;
 
 function preload() {
     table = loadTable("colors.csv", "csv", "header"); 
@@ -6,14 +6,22 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    color = getColor(floor(random(5)));
-    fill(h,s,l)
+    colorMode(HSB, 360, 100, 100);
+    noFill();
+    palette = 0;
 
-
+    let sizeRect = 50;
+    for (let x =0; x<width; x++) {
+        for (let y=0; y< height; y+=sizeRect) {
+            let color = getColor(palette, floor(random(5)));
+            stroke(h,s,b);
+            rect(random(width), random(height), sizeRect);
+        } 
+    }
 }
 
-function getColor(col) {
-    h=int(table.get(0, col*3));
-    s=int(table.get(0, col*3+1));
-    l=int(table.get(0, col*3+2));
+function getColor(palette, col) {
+    h=int(table.get(palette, col*3));
+    s=int(table.get(palette, col*3+1));
+    b=int(table.get(palette, col*3+2));
 }
