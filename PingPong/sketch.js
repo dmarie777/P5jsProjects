bb7a61815157391b3edbb14d8d0e2ecfbc3d06e0
dmarie777 <deimarie777@gmail.com>
2024-06-23 18:09:53 -0500

    Added animation to the ball in the x direction and modified the drawTable function

--- PingPong/sketch.js
+++ PingPong/sketch.js
@@ -9,15 +9,23 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     colorMode(HSB, 360, 100, 100);
     palette = 0;
-    drawTable();
     getColors(palette, floor(random(3,8)));
+    c =3 ;
+    dist=0;
 }
 
 function draw() {
     noStroke();
-    fill(h,s,b)
-    circle(windowWidth/2, windowHeight/2, 50 );
+    drawTable();
+    fill(h,s,b);
+    circle(dist, windowHeight/2, 50 );
+    dist +=c;
+
+    if (dist > width || dist<0) {
+        c *= -1;
+    }  
 }
+
 function getColors(palette, col) {
     h = int(table.get(palette, col*3));
     s = int(table.get(palette, col*3+1));
@@ -25,9 +33,8 @@ function getColors(palette, col) {
 }
 
 function drawTable() {
-    getColors(0,0);
-    background(h,s,b);
-    fill(0);
+    background(int(table.get(0, 0)),int(table.get(0, 1)),int(table.get(0, 2)));
+    noFill();
     stroke('white');
     strokeWeight(5);
     rect(5,5,windowWidth-10, windowHeight-10);
